f4b7baec39e700eb7c5a885382f3d56b82811fa9
robert y <robert@freylax.de>
2023-12-27 20:56:54 +0100

    compiles with zig 0.12

--- build.zig
+++ build.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const gtest_dep = b.dependency("gtest", .{});
 
     const lib = b.addStaticLibrary(.{
         .name = "gtest",
@@ -14,75 +13,51 @@ pub fn build(b: *std.Build) void {
     });
     lib.linkLibCpp();
 
-    const folder = b.build_root.join(b.allocator, &.{ "gtest", "googletest", "src" }) catch @panic("File Not Found");
-    var dir = std.fs.cwd().openIterableDir(folder, .{}) catch unreachable;
-    var it = dir.iterate();
-
-    while (it.next() catch unreachable) |file| {
-        if (file.kind != .file) {
-            continue;
-        }
-
-        const name: []const u8 = file.name;
-        if (std.ascii.endsWithIgnoreCase(name, ".cc")) {
-            lib.addCSourceFile(.{
-                .file = .{ .path = b.pathJoin(&.{ folder, name }) },
-                .flags = &.{
-                    "-Wall",
-                    "-Wshadow",
-                    "-Wconversion",
-                    "-Wundef",
-                    "-Wchar-subscripts",
-                    "-DGTEST_HAS_PTHREAD=0",
-                },
-            });
-        }
+    var cc_files = [_][]const u8{
+        "gtest-all.cc",
+        "gtest-assertion-result.cc",
+        "gtest.cc",
+        "gtest-death-test.cc",
+        "gtest-filepath.cc",
+        "gtest_main.cc",
+        "gtest-matchers.cc",
+        "gtest-port.cc",
+        "gtest-printers.cc",
+        "gtest-test-part.cc",
+        "gtest-typed-test.cc",
+    };
+    for (&cc_files) |*file| {
+        file.* = b.pathJoin(&.{ "googletest", "src", file.* });
     }
-
-    lib.addIncludePath(.{ .path = b.pathJoin(&.{ "gtest", "googletest" }) });
-    lib.addIncludePath(.{ .path = b.pathJoin(&.{ "gtest", "googletest", "include" }) });
-    b.installArtifact(lib);
-
-    lib.step.dependOn(installHeaders(
-        b,
-        b.pathJoin(&.{ "gtest", "googletest", "include", "gtest" }),
-        "gtest",
-    ));
-
-    lib.step.dependOn(installHeaders(
-        b,
-        b.pathJoin(&.{ "gtest", "googletest", "include", "gtest", "internal" }),
-        b.pathJoin(&.{ "gtest", "internal" }),
-    ));
-
-    //TODO should we provide a way to set the custom headers?
-    lib.step.dependOn(installHeaders(
-        b,
-        b.pathJoin(&.{ "gtest", "googletest", "include", "gtest", "internal", "custom" }),
-        b.pathJoin(&.{ "gtest", "internal", "custom" }),
-    ));
-}
-
-fn installHeaders(b: *std.Build, folder: []const u8, out_folder: []const u8) *std.build.Step {
-    var dir = std.fs.cwd().openIterableDir(b.build_root.join(b.allocator, &.{folder}) catch @panic("File Not Found"), .{}) catch unreachable;
-    var it = dir.iterate();
-    var step = b.allocator.create(std.Build.Step) catch @panic("OOM");
-    step.* = std.Build.Step.init(.{
-        .id = .custom,
-        .name = "install headers",
-        .owner = b,
+    lib.addCSourceFiles(.{
+        .dependency = gtest_dep,
+        .files = &cc_files,
+        .flags = &.{
+            "-Wall",
+            "-Wshadow",
+            "-Wconversion",
+            "-Wundef",
+            "-Wchar-subscripts",
+            "-DGTEST_HAS_PTHREAD=0",
+        },
     });
 
-    while (it.next() catch unreachable) |file| {
-        if (file.kind != .file) {
-            continue;
-        }
-
-        const name: []const u8 = file.name;
-        if (std.ascii.endsWithIgnoreCase(name, ".h")) {
-            step.dependOn(&b.addInstallHeaderFile(b.pathJoin(&.{ folder, name }), b.pathJoin(&.{ out_folder, name })).step);
-        }
-    }
-
-    return step;
+    lib.addIncludePath(
+        gtest_dep.path(b.pathJoin(&.{ "googletest", "include" })),
+    );
+    // this is necessary, because likes to include src/gtest-internal-inl.h
+    lib.addIncludePath(
+        gtest_dep.path(b.pathJoin(&.{"googletest"})),
+    );
+    // install the header, we have a LazyPath,
+    // thus can not use installHeader
+    const install_header = b.addInstallFileWithDir(
+        gtest_dep.path(
+            b.pathJoin(&.{ "googletest", "include", "gtest", "gtest.h" }),
+        ),
+        .header,
+        b.pathJoin(&.{ "gtest", "gtest.h" }),
+    );
+    b.getInstallStep().dependOn(&install_header.step);
+    b.installArtifact(lib);
 }
