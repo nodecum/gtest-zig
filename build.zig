dc98cdd2f16540d6e9b552825ed2204306a5fa12
robert y <6729524+freylax@users.noreply.github.com>
2024-01-11 10:22:40 +0100

    added gmock

--- build.zig
+++ build.zig
@@ -4,8 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const gtest_dep = b.dependency("gtest", .{});
-
+    const gt_dep = b.dependency("gtest", .{});
     const cflags = &.{
         "-Wall",
         "-Wshadow",
@@ -14,42 +13,39 @@ pub fn build(b: *std.Build) void {
         "-Wchar-subscripts",
         "-DGTEST_HAS_PTHREAD=0",
     };
-    const gtest_root_dir = gtest_dep.path("googletest");
-    const gtest_include_dir = gtest_dep.path(
-        b.pathJoin(&.{ "googletest", "include" }),
+    // -------------- gtest lib --------------------------------
+    const gtest_root_dir = gt_dep.path("googletest");
+    const gtest_include_dir = gt_dep.path("googletest/include");
+    const gtest_lib = b.addStaticLibrary(
+        .{ .name = "gtest", .target = target, .optimize = optimize },
     );
-    const gtest_lib = b.addStaticLibrary(.{
-        .name = "gtest",
-        .target = target,
-        .optimize = optimize,
-    });
     gtest_lib.linkLibCpp();
-    var cc_files = [_][]const u8{
-        "gtest-assertion-result.cc",
-        "gtest.cc",
-        "gtest-death-test.cc",
-        "gtest-filepath.cc",
-        "gtest-matchers.cc",
-        "gtest-port.cc",
-        "gtest-printers.cc",
-        "gtest-test-part.cc",
-        "gtest-typed-test.cc",
+    const gtest_cc_files = &.{
+        "googletest/src/gtest-assertion-result.cc",
+        "googletest/src/gtest.cc",
+        "googletest/src/gtest-death-test.cc",
+        "googletest/src/gtest-filepath.cc",
+        "googletest/src/gtest-matchers.cc",
+        "googletest/src/gtest-port.cc",
+        "googletest/src/gtest-printers.cc",
+        "googletest/src/gtest-test-part.cc",
+        "googletest/src/gtest-typed-test.cc",
     };
-    // add prefix path to files
-    for (&cc_files) |*file| {
-        file.* = b.pathJoin(&.{ "googletest", "src", file.* });
-    }
-    gtest_lib.addCSourceFiles(.{ .dependency = gtest_dep, .files = &cc_files, .flags = cflags });
+    gtest_lib.addCSourceFiles(
+        .{ .dependency = gt_dep, .files = gtest_cc_files, .flags = cflags },
+    );
     gtest_lib.addIncludePath(gtest_root_dir);
     gtest_lib.addIncludePath(gtest_include_dir);
     b.installArtifact(gtest_lib);
     // we export gtest-all.cc as lib
-    const gtest_all_lib = b.addStaticLibrary(.{ .name = "gtest-all", .target = target, .optimize = optimize });
+    const gtest_all_lib = b.addStaticLibrary(
+        .{ .name = "gtest-all", .target = target, .optimize = optimize },
+    );
     gtest_all_lib.linkLibCpp();
     gtest_all_lib.addCSourceFiles(
         .{
-            .dependency = gtest_dep,
-            .files = &.{b.pathJoin(&.{ "googletest", "src", "gtest-all.cc" })},
+            .dependency = gt_dep,
+            .files = &.{"googletest/src/gtest-all.cc"},
             .flags = cflags,
         },
     );
@@ -58,35 +54,87 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(gtest_all_lib);
 
     // and gtest_main.cc as gtest-main lib
-    const gtest_main_lib = b.addStaticLibrary(.{ .name = "gtest-main", .target = target, .optimize = optimize });
+    const gtest_main_lib = b.addStaticLibrary(
+        .{ .name = "gtest-main", .target = target, .optimize = optimize },
+    );
     gtest_main_lib.linkLibCpp();
     gtest_main_lib.addCSourceFiles(
         .{
-            .dependency = gtest_dep,
-            .files = &.{b.pathJoin(&.{ "googletest", "src", "gtest_main.cc" })},
+            .dependency = gt_dep,
+            .files = &.{"googletest/src/gtest_main.cc"},
             .flags = cflags,
         },
     );
     gtest_main_lib.addIncludePath(gtest_root_dir);
     gtest_main_lib.addIncludePath(gtest_include_dir);
     b.installArtifact(gtest_main_lib);
-
-    // install the header, we have a LazyPath,
-    // thus can not use installHeader
-    // const install_gtest_header = b.addInstallFileWithDir(
-    //     gtest_dep.path(
-    //         b.pathJoin(&.{ "googletest", "include", "gtest", "gtest.h" }),
-    //     ),
-    //     .header,
-    //     b.pathJoin(&.{ "gtest", "gtest.h" }),
-    // );
-    // b.getInstallStep().dependOn(&install_gtest_header.step);
-    // gtest_lib.installed_headers.append(&install_gtest_header.step) catch @panic("OOM");
     gtest_lib.installHeadersDirectoryOptions(
         .{
-            .source_dir = gtest_dep.path(b.pathJoin(&.{ "googletest", "include", "gtest" })),
+            .source_dir = gt_dep.path("googletest/include/gtest"),
             .install_dir = .header,
             .install_subdir = "gtest",
+        },
+    );
+    // --------------- gmock --------------------------------------------
+    const gmock_root_dir = gt_dep.path("googlemock");
+    const gmock_include_dir = gt_dep.path("googlemock/include");
+
+    const gmock_lib = b.addStaticLibrary(
+        .{ .name = "gmock", .target = target, .optimize = optimize },
+    );
+    gmock_lib.linkLibCpp();
+    const gmock_cc_files = &.{
+        "googlemock/src/gmock-cardinalities.cc",
+        "googlemock/src/gmock.cc",
+        "googlemock/src/gmock-internal-utils.cc",
+        "googlemock/src/gmock-matchers.cc",
+        "googlemock/src/gmock-spec-builders.cc",
+    };
+    gmock_lib.addCSourceFiles(
+        .{ .dependency = gt_dep, .files = gmock_cc_files, .flags = cflags },
+    );
+    gmock_lib.addIncludePath(gmock_root_dir);
+    gmock_lib.addIncludePath(gtest_include_dir);
+    gmock_lib.addIncludePath(gmock_include_dir);
+    b.installArtifact(gmock_lib);
+    // we export gmock-all.cc as lib
+    const gmock_all_lib = b.addStaticLibrary(
+        .{ .name = "gmock-all", .target = target, .optimize = optimize },
+    );
+    gmock_all_lib.linkLibCpp();
+    gmock_all_lib.addCSourceFiles(
+        .{
+            .dependency = gt_dep,
+            .files = &.{"googlemock/src/gmock-all.cc"},
+            .flags = cflags,
+        },
+    );
+    gmock_all_lib.addIncludePath(gmock_root_dir);
+    gmock_all_lib.addIncludePath(gtest_include_dir);
+    gmock_all_lib.addIncludePath(gmock_include_dir);
+    b.installArtifact(gmock_all_lib);
+
+    // and gmock_main.cc as gmock-main lib
+    const gmock_main_lib = b.addStaticLibrary(
+        .{ .name = "gmock-main", .target = target, .optimize = optimize },
+    );
+    gmock_main_lib.linkLibCpp();
+    gmock_main_lib.addCSourceFiles(
+        .{
+            .dependency = gt_dep,
+            .files = &.{"googlemock/src/gmock_main.cc"},
+            .flags = cflags,
+        },
+    );
+    gmock_main_lib.addIncludePath(gmock_root_dir);
+    gmock_main_lib.addIncludePath(gtest_include_dir);
+    gmock_main_lib.addIncludePath(gmock_include_dir);
+    b.installArtifact(gmock_main_lib);
+    gmock_lib.installHeadersDirectoryOptions(
+        .{
+            .source_dir = gt_dep.path("googlemock/include/gmock"),
+            .install_dir = .header,
+            .install_subdir = "gmock",
         },
     );
 }
